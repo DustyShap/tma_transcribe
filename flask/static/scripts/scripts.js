5d6bb8566295ef0439b04afbf10ea87e646d83c8
Dustin Shaprio <dustin.shapiro@spoton.com>
2023-12-27 16:27:49 -0500

    Frontend

---
+++ flask/static/scripts/scripts.js
@@ -0,0 +1,23 @@
+function toggleVisibility(segmentId) {
+    var content = document.getElementById(segmentId);
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+function filterByDate() {
+    var startDate = document.getElementById('startDate').value;
+    var endDate = document.getElementById('endDate').value;
+    var transcriptions = document.getElementById('transcriptions').children;
+
+    for (var i = 0; i < transcriptions.length; i++) {
+        var transcriptionDate = transcriptions[i].getAttribute('data-date');
+
+        if (transcriptionDate >= startDate && transcriptionDate <= endDate) {
+            transcriptions[i].style.display = '';
+        } else {
+            transcriptions[i].style.display = 'none';
+        }
+    }
+}
