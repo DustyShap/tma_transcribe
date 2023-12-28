ecca9695cb3bcd395a8b330721bb1dd16721c650
Ubuntu <ubuntu@ip-172-31-26-118.us-west-2.compute.internal>
2023-12-28 17:04:28 +0000

    Query and serve

--- flask/app.py
+++ flask/app.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from sqlalchemy import desc
+
 
 from models import Transcription
 from create import create_app
 
 app = create_app()
 app.app_context().push()
+
 @app.route('/')
 def hello_world():
-    all_transcriptions = Transcription.query.all()
-    return render_template('index.html')
+    all_transcriptions = Transcription.query.order_by(desc(Transcription.segment_pub_date), Transcription.segment_title).all()
+
+    transcription_list = [transcription for transcription in all_transcriptions]
+    return render_template('index.html', transcriptions=transcription_list)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8082)
+  app.run(host='0.0.0.0', port=8080)
