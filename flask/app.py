73b3e4ca4f65c0997d818d854afacc60e8f0df52
Dustin Shaprio <dustin.shapiro@spoton.com>
2023-12-28 12:07:15 -0500

    Pagination

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, jsonify
 from sqlalchemy import desc
-
-
 from models import Transcription
 from create import create_app
 
@@ -10,10 +8,13 @@ app.app_context().push()
 
 @app.route('/')
 def hello_world():
-    all_transcriptions = Transcription.query.order_by(desc(Transcription.segment_pub_date), Transcription.segment_title).all()
+    page = request.args.get('page', 1, type=int)
+    per_page = 10  # Set the number of items per page
 
-    transcription_list = [transcription for transcription in all_transcriptions]
-    return render_template('index.html', transcriptions=transcription_list)
+    paginated_transcriptions = Transcription.query.order_by(desc(Transcription.segment_pub_date), Transcription.segment_title).paginate(page, per_page, False)
+    transcription_list = [transcription for transcription in paginated_transcriptions.items]
+
+    return render_template('index.html', transcriptions=transcription_list, pagination=paginated_transcriptions)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
