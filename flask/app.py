6e189d0e528747dc3d5f981065431e8eea168d23
Ubuntu <ubuntu@ip-172-31-26-118.us-west-2.compute.internal>
2023-12-28 17:22:53 +0000

    Updates

--- flask/app.py
+++ flask/app.py
@@ -11,10 +11,11 @@ def hello_world():
     page = request.args.get('page', 1, type=int)
     per_page = 10  # Set the number of items per page
 
-    paginated_transcriptions = Transcription.query.order_by(desc(Transcription.segment_pub_date), Transcription.segment_title).paginate(page, per_page, False)
+    paginated_transcriptions = Transcription.query.order_by(desc(Transcription.segment_pub_date), Transcription.segment_title).paginate(page=page, per_page=per_page, error_out=False)
     transcription_list = [transcription for transcription in paginated_transcriptions.items]
 
     return render_template('index.html', transcriptions=transcription_list, pagination=paginated_transcriptions)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
+
